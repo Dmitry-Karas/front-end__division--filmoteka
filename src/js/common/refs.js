14058b693c4e7d88e155af196f04bdc0c053f4ea
Dmitry Karas <dmitrythekaras@gmail.com>
2021-05-29 12:23:39 +0300

    added logic and some styles

--- src/js/common/refs.js
+++ src/js/common/refs.js
@@ -4,3 +4,4 @@ export const navListRef = document.querySelector('.js-navigation-list');
 export const homeButtonRef = document.querySelector('[data-button="home"]');
 export const libraryButtonRef = document.querySelector('[data-button="library"]');
 export const headerDynamicContainerRef = document.querySelector('header .js-dynamic-container');
+export const themeSwitcherRef = document.querySelector('#theme-switch-toggle');
