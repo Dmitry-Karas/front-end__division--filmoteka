f7d305361a0238c1cadcdcade26e6196c1508579
Oleksandr Yakovenko <mozg555666@gmail.com>
2021-06-09 16:44:19 +0300

    refactoring code and fix busg

--- src/js/team.js
+++ src/js/team.js
@@ -2,50 +2,43 @@ export default [
   {
     name: 'Oleksandr',
     surname: 'Vasylchuk',
-    photo:
-      'https://pixabay.com/get/gceaefbc1eb7ef8ac4d404064dc1345f0512fa6dac49f1c80fd92428ac12fca8349d8fd1a4a49eecdc3da3228a8e5310865ec28fc5dd17d4cc74cf04b477ebd28_640.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2014/07/23/16/05/panda-400277_960_720.jpg',
     github: 'https://github.com/OleksandrVasylchuk',
   },
   {
     name: 'Olena',
     surname: 'Pavliuk',
-    photo:
-      'https://pixabay.com/get/gceaefbc1eb7ef8ac4d404064dc1345f0512fa6dac49f1c80fd92428ac12fca8349d8fd1a4a49eecdc3da3228a8e5310865ec28fc5dd17d4cc74cf04b477ebd28_640.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2014/07/23/16/05/panda-400277_960_720.jpg',
     github: 'https://github.com/Olena-P',
   },
   {
     name: 'Oleksandr',
     surname: 'Yakovenko',
-    photo:
-      'https://pixabay.com/get/gceaefbc1eb7ef8ac4d404064dc1345f0512fa6dac49f1c80fd92428ac12fca8349d8fd1a4a49eecdc3da3228a8e5310865ec28fc5dd17d4cc74cf04b477ebd28_640.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2014/07/23/16/05/panda-400277_960_720.jpg',
     github: 'https://github.com/Reversus28',
   },
   {
     name: 'Anastasia',
     surname: 'Aleksandrova',
-    photo:
-      'https://pixabay.com/get/gceaefbc1eb7ef8ac4d404064dc1345f0512fa6dac49f1c80fd92428ac12fca8349d8fd1a4a49eecdc3da3228a8e5310865ec28fc5dd17d4cc74cf04b477ebd28_640.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2014/07/23/16/05/panda-400277_960_720.jpg',
     github: 'https://github.com/Anastasia-A-2020',
   },
   {
     name: 'Dmitry',
     surname: 'Karas',
-    photo:
-      'https://pixabay.com/get/gceaefbc1eb7ef8ac4d404064dc1345f0512fa6dac49f1c80fd92428ac12fca8349d8fd1a4a49eecdc3da3228a8e5310865ec28fc5dd17d4cc74cf04b477ebd28_640.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2014/07/23/16/05/panda-400277_960_720.jpg',
     github: 'https://github.com/dmitry-karas',
   },
   {
     name: 'Olga',
     surname: 'Бездетко',
-    photo:
-      'https://pixabay.com/get/gceaefbc1eb7ef8ac4d404064dc1345f0512fa6dac49f1c80fd92428ac12fca8349d8fd1a4a49eecdc3da3228a8e5310865ec28fc5dd17d4cc74cf04b477ebd28_640.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2014/07/23/16/05/panda-400277_960_720.jpg',
     github: '',
   },
   {
     name: 'Maryna',
     surname: 'Berezhanska',
-    photo:
-      'https://pixabay.com/get/gceaefbc1eb7ef8ac4d404064dc1345f0512fa6dac49f1c80fd92428ac12fca8349d8fd1a4a49eecdc3da3228a8e5310865ec28fc5dd17d4cc74cf04b477ebd28_640.jpg',
+    photo: 'https://cdn.pixabay.com/photo/2014/07/23/16/05/panda-400277_960_720.jpg',
     github: '',
   },
 ];
