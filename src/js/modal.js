3ae647fea3cc0f9cf14a35c7b9de20f3fc36bc3a
Oleksandr Yakovenko <mozg555666@gmail.com>
2021-06-16 22:12:23 +0300

    fix

--- src/js/modal.js
+++ src/js/modal.js
@@ -115,7 +115,6 @@ async function addTrailerToModal(e) {
 
       const parent = document.querySelector('.carousel-inner');
       parent.firstElementChild.classList.add('active');
-
       stopSliderModal();
     }
     if (trailer.length === 1) {
@@ -137,63 +136,6 @@ function stopSliderModal() {
   const myCarousel = document.querySelector('#carouselExampleControls');
   const carousel = new bootstrap.Carousel(myCarousel, {
     pause: true,
+    interval: 2000,
   });
 }
-
-// async function openModalTrailer(e) {
-//   try {
-//     if (e.target.classList.contains('movie__button-youtube')) {
-//       const id = e.target.getAttribute('data-movie-id');
-//       const trailer = await newFetchApiFilms.fetchTrailerById(id).then(res => res.data.results); //делает запрос
-
-//       console.log('trailer', trailer.length);
-
-//       const backdrop = document.querySelector('.trailer__overlay');
-
-//       if (trailer.length > 0) {
-//         backdrop.classList.remove('is-hidden');
-//         renderMarkup(trailerModalRef, trailerFilmTmp(trailer));
-
-//         stopSliderModal();
-//         const parent = document.querySelector('.carousel-inner');
-//         parent.firstElementChild.classList.add('active');
-//       }
-//       if (trailer.length === 1) {
-//         const controllerPrev = document.querySelector('.carousel-control-prev');
-//         const controllerNext = document.querySelector('.carousel-control-next');
-//         controllerPrev.classList.add('visually-hidden');
-//         controllerNext.classList.add('visually-hidden');
-//       }
-//
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-// //
-
-// function onOverlayClick(e) {
-//   if (e.target === e.currentTarget) {
-//     closeModal();
-//   }
-// }
-
-// function onPressEscKey(e) {
-//   const ESC_KEY_CODE = 'Escape';
-
-//   if (e.code === ESC_KEY_CODE) {
-//     closeModal();
-//   }
-// }
-
-// function closeModal() {
-//   trailerBackdropRef.classList.add('is-hidden');
-//   clearMarkup(trailerModalRef);
-// }
-
-// function stopSliderModal() {
-//   const myCarousel = document.querySelector('#carouselExampleControls');
-//   const carousel = new bootstrap.Carousel(myCarousel, {
-//     pause: true,
-//   });
-// }
--- src/js/trailer.js
+++ src/js/trailer.js
@@ -1,66 +1,66 @@
-import NewFetchApiFilms from './apiService';
-import trailerFilmTmp from '../templates/trailer.hbs';
-import { renderMarkup, clearMarkup } from './common/functions';
-import { listFilmsRef, trailerModalRef, trailerBackdropRef } from './common/refs';
+// import NewFetchApiFilms from './apiService';
+// import trailerFilmTmp from '../templates/trailer.hbs';
+// import { renderMarkup, clearMarkup } from './common/functions';
+// import { listFilmsRef, trailerModalRef, trailerBackdropRef } from './common/refs';
 
-const newFetchApiFilms = new NewFetchApiFilms();
+// const newFetchApiFilms = new NewFetchApiFilms();
 
-listFilmsRef.addEventListener('click', openModalTrailer);
+// listFilmsRef.addEventListener('click', openModalTrailer);
 
-window.addEventListener('keydown', onPressEscKey);
+// window.addEventListener('keydown', onPressEscKey);
 
-async function openModalTrailer(e) {
-  try {
-    if (e.target.classList.contains('movie__button-youtube')) {
-      const id = e.target.getAttribute('data-movie-id');
-      const trailer = await newFetchApiFilms.fetchTrailerById(id).then(res => res.data.results); //делает запрос
+// async function openModalTrailer(e) {
+//   try {
+//     if (e.target.classList.contains('movie__button-youtube')) {
+//       const id = e.target.getAttribute('data-movie-id');
+//       const trailer = await newFetchApiFilms.fetchTrailerById(id).then(res => res.data.results); //делает запрос
 
-      console.log('trailer', trailer.length);
+//       console.log('trailer', trailer.length);
 
-      const backdrop = document.querySelector('.trailer__overlay');
+//       const backdrop = document.querySelector('.trailer__overlay');
 
-      if (trailer.length > 0) {
-        backdrop.classList.remove('is-hidden');
-        renderMarkup(trailerModalRef, trailerFilmTmp(trailer));
+//       if (trailer.length > 0) {
+//         backdrop.classList.remove('is-hidden');
+//         renderMarkup(trailerModalRef, trailerFilmTmp(trailer));
 
-        stopSliderModal();
-        const parent = document.querySelector('.carousel-inner');
-        parent.firstElementChild.classList.add('active');
-      }
-      if (trailer.length === 1) {
-        const controllerPrev = document.querySelector('.carousel-control-prev');
-        const controllerNext = document.querySelector('.carousel-control-next');
-        controllerPrev.classList.add('visually-hidden');
-        controllerNext.classList.add('visually-hidden');
-      }
-      if (trailer.length === 0) {
-        const lol = document.querySelector('movie__button-youtube');
-        console.log('lol', lol);
-        backdrop.classList.remove('is-hidden');
-        renderMarkup(trailerModalRef, trailerFilmTmp(trailer));
-      }
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
-//
+//         stopSliderModal();
+//         const parent = document.querySelector('.carousel-inner');
+//         parent.firstElementChild.classList.add('active');
+//       }
+//       if (trailer.length === 1) {
+//         const controllerPrev = document.querySelector('.carousel-control-prev');
+//         const controllerNext = document.querySelector('.carousel-control-next');
+//         controllerPrev.classList.add('visually-hidden');
+//         controllerNext.classList.add('visually-hidden');
+//       }
+//       if (trailer.length === 0) {
+//         const lol = document.querySelector('movie__button-youtube');
+//         console.log('lol', lol);
+//         backdrop.classList.remove('is-hidden');
+//         renderMarkup(trailerModalRef, trailerFilmTmp(trailer));
+//       }
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// //
 
-function onOverlayClick(e) {
-  if (e.target === e.currentTarget) {
-    closeModal();
-  }
-}
+// function onOverlayClick(e) {
+//   if (e.target === e.currentTarget) {
+//     closeModal();
+//   }
+// }
 
-function onPressEscKey(e) {
-  const ESC_KEY_CODE = 'Escape';
+// function onPressEscKey(e) {
+//   const ESC_KEY_CODE = 'Escape';
 
-  if (e.code === ESC_KEY_CODE) {
-    closeModal();
-  }
-}
+//   if (e.code === ESC_KEY_CODE) {
+//     closeModal();
+//   }
+// }
 
-function closeModal() {
-  trailerBackdropRef.classList.add('is-hidden');
-  clearMarkup(trailerModalRef);
-}
+// function closeModal() {
+//   trailerBackdropRef.classList.add('is-hidden');
+//   clearMarkup(trailerModalRef);
+// }
